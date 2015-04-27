2f4fd9e623b91094b8d121d73a1ef804a6e2e87b
王小龙 <1036199213@qq.com>
2015-04-27 17:35:20 +0800

    检测空赋值默认

--- Magic.Mvc/Check.cs
+++ Magic.Mvc/Check.cs
@@ -22,5 +22,18 @@ namespace Magic.Mvc
             if (values.Any(m => m == null)) throw new NullReferenceException();
         }
 
+        /// <summary>
+        /// 如果对象为空，则赋值默认值
+        /// </summary>
+        /// <typeparam name="T">值类型</typeparam>
+        /// <param name="obj">值</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns></returns>
+        public static T IsNullOrDefault<T>(T obj, T defaultValue)
+        {
+            if (null == obj)
+                return defaultValue;
+            return obj;
+        }
     }
 }
